4b81bf04932fca7fd70ca2b60f5004f126017792
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-04-28 23:45:40 +0000

    change 2 files

--- js/script.js
+++ js/script.js
@@ -15,16 +15,47 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function converts the degrees from fahrenheit to celsius
+ * This function calculates the cost of a pizza
  */
-function convert() {
+function myButtonClicked() {
   // input
-  const fahrenheit = parseInt(document.getElementById("fahrenheit").value)
+  const sL = document.getElementById("size-large").checked
+  const sXL = document.getElementById("size-extra-large").checked
+  const oT = document.getElementById("one").checked
+  const tT = document.getElementById("two").checked
+  const thrT = document.getElementById("three").checked
+  const fT = document.getElementById("four").checked
+  var size
+  var toppings
 
-  // process
-  const celsius = ((fahrenheit - 32) * 5) / 9
+  // process and output
+  const TAX = 0.13
 
-  // output
-  document.getElementById("celsius").innerHTML =
-    "<p>The  temperature in celsius is: " + celsius.toFixed(2) + "°<p>"
+  if (sL == true) {
+    size = 6.0
+  }
+
+  if (sL == false) {
+    size = 10.0
+  }
+
+  if (oT == true) {
+    toppings = 1.0
+  }
+
+  if (tT == true) {
+    toppings = 1.75
+  }
+
+  if (thrT == true) {
+    toppings = 2.5
+  }
+
+  if (fT == true) {
+    toppings = 3.35
+  }
+
+  var total = (size + toppings) * (1 + TAX)
+  document.getElementById("cost").innerHTML =
+    "The total cost is $" + total.toFixed(2)
 }
